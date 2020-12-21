1a30ec0a045f9d8724b663ed623dc76a6233826d
malte-L <lehmannmalte@gmx.de>
2020-12-21 15:41:27 +0100

    Now checking for unmonitored changes to the fan device

--- gofan.go
+++ gofan.go
@@ -4,8 +4,14 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"time"
+)
+
+const (
+	dev            = "/proc/acpi/ibm/fan"
+	configFileName = "gofan"
 )
 
 func applyLevel(level string) error {
@@ -13,10 +19,9 @@ func applyLevel(level string) error {
 
 	stdin := &bytes.Buffer{}
 
-	dev := "/proc/acpi/ibm/fan"
 	tee := exec.Command("tee", dev)
 	tee.Stdin = stdin
-	tee.Stderr = log.Writer()
+	tee.Stderr = os.Stderr
 	tee.Stdout = log.Writer()
 
 	stdin.WriteString("level " + level + "\n")
@@ -33,6 +38,7 @@ func checkIfShouldDowngrade(transitionWhenBelow float32, timeout time.Duration) 
 			temp, err := getTemp()
 			if err != nil {
 				callback <- err
+				return
 			}
 
 			if temp > transitionWhenBelow {
@@ -50,11 +56,12 @@ func checkIfShouldUpgrade(ruleset *Ruleset, currentModeName string, timeout time
 	c := make(chan error)
 	ticker := time.NewTicker(timeout)
 
-	go func(ruleset *Ruleset, currentModeName string, ticker *time.Ticker) {
+	go func(ruleset *Ruleset, currentModeName string, callback chan<- error, ticker *time.Ticker) {
 		for range ticker.C {
 			temp, err := getTemp()
 			if err != nil {
 				c <- err
+				return
 			}
 
 			mode, err := ruleset.findAppropriateMode(temp)
@@ -66,14 +73,38 @@ func checkIfShouldUpgrade(ruleset *Ruleset, currentModeName string, timeout time
 
 			c <- nil
 		}
-	}(ruleset, currentModeName, ticker)
+	}(ruleset, currentModeName, c, ticker)
 
 	return c
 }
 
 // sometimes my os decides it's time for lift-off; even when the current cpu temperatur (and Elon Musk) says it's not. This occurs often when AC is plugged in
-func fanToFastForCurrentTemp() <-chan error {
+// in this case the current level as given by reading from the fan device does not equal the level of the mode we're currently in. In this case we should trigger a recheck on the currently best suited mode
+func checkForUnmonitoredDeviceChanges(wantedCurrentLevel string) <-chan error {
 	c := make(chan error)
+
+	timout := time.Second * 10
+
+	ticker := time.NewTicker(timout)
+
+	go func(wantedCurrentLevel string, callback chan<- error, ticker *time.Ticker) {
+		for range ticker.C {
+
+			level, err := GetCurrentFanLevel(dev)
+			if err != nil {
+				c <- err
+				return
+			}
+
+			if level == wantedCurrentLevel {
+				continue
+			}
+
+			fmt.Fprintf(Notify, "fan was too fast")
+			c <- nil
+		}
+	}(wantedCurrentLevel, c, ticker)
+
 	return c
 }
 
@@ -82,7 +113,6 @@ func logic(ruleset *Ruleset) error {
 	var mode *Mode
 
 	for {
-		log.Printf("\n")
 		log.Printf("get temperatur information..")
 		temp, err := getTemp()
 		if err != nil {
@@ -101,13 +131,15 @@ func logic(ruleset *Ruleset) error {
 			return err
 		}
 		log.Printf("success\n")
+		log.Printf("\n")
 
 		select {
 		case err = <-checkIfShouldDowngrade(float32(mode.TransitionWhenBelow), ruleset.Timeouts.Downgrade.Duration):
 			log.Printf("should downgrade\n")
 		case err = <-checkIfShouldUpgrade(ruleset, mode.Name, ruleset.Timeouts.Upgrade.Duration):
 			log.Printf("should upgrade\n")
-		case err = <-fanToFastForCurrentTemp():
+		case err = <-checkForUnmonitoredDeviceChanges(mode.Level):
+			log.Printf("umonitored device change\n")
 		case <-time.After(ruleset.Timeouts.Standard.Duration):
 			log.Printf("timed out after %v\n", ruleset.Timeouts.Standard.Duration)
 		}
@@ -115,6 +147,7 @@ func logic(ruleset *Ruleset) error {
 		if err != nil {
 			return err
 		}
+
 	}
 }
 
